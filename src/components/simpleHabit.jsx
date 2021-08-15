5f6be7042a4df9534ba8289cd1174e483639c9dd
Casey <hjkdw9506@gmail.com>
2021-08-15 12:52:25 +0900

    🎨 React Hook "setState" 연습

--- src/components/simpleHabit.jsx
+++ src/components/simpleHabit.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SimpleHabit = (props) => {
-  let count = 0;
+  const [count, setCount] = useState(0); // useState를 사용하면, class에서 생성한 state정보를 가져올 수 있다.
+  // useState를 사용할 때는 const 변수를 2가지 지정해줘야 한다 1.실제 state 값과, 2.state를 업데이트 할 수 있는 setState같은 함수를 반환한다
 
   const handleIncrement = () => {
-    ++count;
+    setCount(count + 1); // 오브젝트로 인자 넣어주는 것이 아니다!
   };
 
   return (
